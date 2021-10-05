87841d93819f2daf7a3c0da95b6939050049c445
LukeDraper <lukewdraper@gmail.com>
2021-10-05 16:41:05 -0600

    Added stuff to test mongodb on Heroku

--- app.js
+++ app.js
@@ -1,26 +1,73 @@
 const path = require('path');
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
-const bodyParser = require('body-parser');
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views'); // This is the default setting.
 
-const adminData = require('./routes/admin');
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+
+const corsOptions = {
+    origin: "https://arcane-ravine-67307.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://cse341:<WebBackend123>@cluster0.q8tvx.mongodb.net/testDatabase?retryWrites=true&w=majority";
+                        
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
-app.use(shopRoutes);
-
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'})
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.listen(port);
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
