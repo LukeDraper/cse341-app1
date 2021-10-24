8bc6da6d0b33c63f39917e086690770fa605de30
LukeDraper <lukewdraper@gmail.com>
2021-10-24 01:49:10 -0600

    Imported User model into auth route to check if user exists.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { check, body } = require('express-validator/check');
 
+const User = require('../models/user');
+
 const authController = require('../controllers/auth');
 
 const router = express.Router();
